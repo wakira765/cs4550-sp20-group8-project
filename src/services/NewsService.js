29dff0908182cc51bc5329ebbda38712a26331f3
Danijj <danijj89@gmail.com>
2020-04-16 13:24:59 -0400

    added service to fetch recalls by date range

--- src/services/NewsService.js
+++ src/services/NewsService.js
@@ -17,8 +17,23 @@ export const findNewsBySearchTerm = (searchTerm) => {
         .then(response => response.json())
 };
 
+/**
+ * Finds recalls given to date ranges.
+ * The format of the from and to must be: 'yyyymmdd'.
+ * Example: Jan 31st 2020 => 20200131
+ * @param from
+ * @param to
+ * @returns {Promise<any>}
+ */
+export const findRecallsByDateRange = (from, to) => {
+    const range = `${from}+${to}`;
+    return fetch(`${LOCALHOST_API}/recalls/${range}`)
+        .then(response => response.json())
+};
+
 export default {
     findLatestNews,
     getLatestRecalls,
-    findNewsBySearchTerm
+    findNewsBySearchTerm,
+    findRecallsByDateRange
 }
